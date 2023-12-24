f93e6e6bb2dfa2fec2eb0d57016d28dfcf7858b6
nibezo <nibezo.cs@gmail.com>
2023-12-25 00:51:12 +0400

    feat: Add useState hook to all utils

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import React, {useState } from 'react';
 import { Switch } from 'antd';
 
 function App() {
+  let [electricity, setElectricity] = useState(false);
+  let [gas, setGas] = useState(false);
+  let [water, setWater] = useState(false);
+
+  function handleClickOfElectricity() {
+    setElectricity(electricity ? false : true);
+  }
+  function handleClickOfGas() {
+    setGas(gas ? false : true);
+  }
+  function handleClickOfWater() {
+    setWater(water ? false : true);
+  }
   return (
     <div className="App">
       <h1>Armenian utilities control panel</h1>
       <div className="Util">
-        <Switch size="large" style={{ margin: 10}}/>
+        <Switch checked={electricity} size="large" style={{ margin: 10}} onClick={handleClickOfElectricity}/>
         <p>Electricity</p>
       </div>
       <div className="Util">
-        <Switch size="large" style={{ margin: 10}}/>
+        <Switch checked={gas} size="large" style={{ margin: 10}} onClick={handleClickOfGas}/>
         <p>Gas</p>
       </div>
       <div className="Util">
-        <Switch size="large" style={{ margin: 10}}/>
+        <Switch checked={water} size="large" style={{ margin: 10}} onClick={handleClickOfWater}/>
         <p>Water</p>
       </div>
     </div>
